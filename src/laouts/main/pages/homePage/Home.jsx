68111b6c1284265cf1752de53b648737592eb77c
Faezeh-Karami <fk96karami@gmail.com>
2024-05-18 19:52:27 +0330

    create a component for slideShow

--- src/laouts/main/pages/homePage/Home.jsx
+++ src/laouts/main/pages/homePage/Home.jsx
@@ -1,45 +1,10 @@
 import React from "react";
-import { Slide } from "react-slideshow-image";
-import "react-slideshow-image/dist/styles.css";
-import slid1 from "./slideShowPic/american-literature.jpg";
-import slid2 from "./slideShowPic/plays-2.jpg";
-import slid3 from "./slideShowPic/plays.jpg";
-import slid4 from "./slideShowPic/writers_1000.jpg";
-import "./home.scss";
+import Slider from "./homeComponents/Slider";
+
 export default function Home() {
   return (
     <>
-      <Slide>
-        <div className="each-slide-effect">
-          <div>
-            <span>
-              <img src={slid1} alt="slid1" />
-            </span>
-          </div>
-        </div>
-        <div className="each-slide-effect">
-          <div>
-            <span>
-              <img src={slid2} alt="slid2" />
-            </span>
-          </div>
-        </div>
-        <div className="each-slide-effect">
-          <div>
-            <span>
-              <img src={slid3} alt="slid3" />
-            </span>
-          </div>
-        </div>
-        <div className="each-slide-effect">
-          <div>
-            <span>
-              <img src={slid4} alt="slid4" />
-            </span>
-          </div>
-        </div>
-        ّ
-      </Slide>
+      <Slider />
     </>
   );
 }
