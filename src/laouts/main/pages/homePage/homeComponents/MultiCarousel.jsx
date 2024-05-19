43a19bea74bd73644c1523a45f60fc42b6038b36
Faezeh-Karami <fk96karami@gmail.com>
2024-05-20 00:17:31 +0330

    edit multiCarousel

--- src/laouts/main/pages/homePage/homeComponents/MultiCarousel.jsx
+++ src/laouts/main/pages/homePage/homeComponents/MultiCarousel.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import slid1 from "../slideShowPic/american-literature.jpg";
-import slid2 from "../slideShowPic/plays-2.jpg";
-import slid3 from "../slideShowPic/plays.jpg";
-import slid4 from "../slideShowPic/writers_1000.jpg";
 import "../home.scss";
+import { useSelector } from "react-redux";
+
 export default function MultiCarousel() {
+
+  const books = useSelector((state) => state.shop.books);
   const responsive = {
     superLargeDesktop: {
       breakpoint: { max: 4000, min: 3000 },
@@ -27,36 +27,15 @@ export default function MultiCarousel() {
   };
   return (
     <Carousel responsive={responsive}>
-      <div>
-        <img src={slid2} alt="pic2" className="imgCarouselStyle" />
-      </div>
-      <div>
-        <img src={slid3} alt="pic3" className="imgCarouselStyle" />
-      </div>
-      <div>
-        <img src={slid4} alt="pic4" className="imgCarouselStyle" />
-      </div>
-      <div>
-        <img src={slid1} alt="pic1" className="imgCarouselStyle" />
-      </div>
-      <div>
-        <img src={slid2} alt="pic2" className="imgCarouselStyle" />
-      </div>
-      <div>
-        <img src={slid3} alt="pic3" className="imgCarouselStyle" />
-      </div>
-      <div>
-        <img src={slid4} alt="pic4" className="imgCarouselStyle" />
-      </div>
-      <div>
-        <img src={slid2} alt="pic2" className="imgCarouselStyle" />
-      </div>
-      <div>
-        <img src={slid3} alt="pic3" className="imgCarouselStyle" />
-      </div>
-      <div>
-        <img src={slid4} alt="pic4" className="imgCarouselStyle" />
-      </div>
+      {books.map((book) => (
+        <div key={book.id} className="backCarouselStyle" >
+          <img
+            src={book.picture}
+            alt={book.title}
+          />
+        </div>
+      ))}
+
     </Carousel>
   );
 }
--- src/shopSlice.js
+++ src/shopSlice.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
-import pic2 from "./bookPic/ابله.jpg";
 import pic1 from "./bookPic/آخرین غول.jpg";
+import pic2 from "./bookPic/ابله.jpg";
 import pic3 from "./bookPic/جزیره.jpg";
 import pic4 from "./bookPic/خاکستر.jpg";
 import pic5 from "./bookPic/خیابان-تمساح-ها.jpg";
@@ -20,25 +20,25 @@ import pic18 from "./bookPic/کمدی-در-گذر-زمان-.jpg";
 import pic19 from "./bookPic/کنفسیوس.jpg";
 const initialState = {
   books: [
-    { title: "ghool", id: 1, price: 1000, count: 1, picture: { pic1 } },
-    { title: "ablah", id: 2, price: 1000, count: 1, picture: { pic2 } },
-    { title: "jazireh", id: 3, price: 1000, count: 1, picture: { pic3 } },
-    { title: "khakestar", id: 4, price: 1000, count: 1, picture: { pic4 } },
-    { title: "temsah", id: 5, price: 1000, count: 1, picture: { pic5 } },
-    { title: "ozala", id: 6, price: 1000, count: 1, picture: { pic6 } },
-    { title: "palid", id: 7, price: 1000, count: 1, picture: { pic7 } },
-    { title: "sargarmi", id: 8, price: 1000, count: 1, picture: { pic8 } },
-    { title: "shetaban", id: 9, price: 1000, count: 1, picture: { pic9 } },
-    { title: "parm", id: 10, price: 1000, count: 1, picture: { pic10 } },
-    { title: "farzandan", id: 11, price: 1000, count: 1, picture: { pic11 } },
-    { title: "lamber", id: 12, price: 1000, count: 1, picture: { pic12 } },
-    { title: "madam", id: 13, price: 1000, count: 1, picture: { pic13 } },
-    { title: "mashaal", id: 14, price: 1000, count: 1, picture: { pic14 } },
-    { title: "mojezeh", id: 15, price: 1000, count: 1, picture: { pic15 } },
-    { title: "jerminal", id: 16, price: 1000, count: 1, picture: { pic16 } },
-    { title: "vatican", id: 17, price: 1000, count: 1, picture: { pic17 } },
-    { title: "comedy", id: 18, price: 1000, count: 1, picture: { pic18 } },
-    { title: "confocious", id: 19, price: 1000, count: 1, picture: { pic19 } },
+    { title: "ghool", id: 1, price: 1000, count: 1, picture: pic1 },
+    { title: "ablah", id: 2, price: 1000, count: 1, picture: pic2 },
+    { title: "jazireh", id: 3, price: 1000, count: 1, picture: pic3 },
+    { title: "khakestar", id: 4, price: 1000, count: 1, picture: pic4 },
+    { title: "temsah", id: 5, price: 1000, count: 1, picture: pic5 },
+    { title: "ozala", id: 6, price: 1000, count: 1, picture: pic6 },
+    { title: "palid", id: 7, price: 1000, count: 1, picture: pic7 },
+    { title: "sargarmi", id: 8, price: 1000, count: 1, picture: pic8 },
+    { title: "shetaban", id: 9, price: 1000, count: 1, picture: pic9 },
+    { title: "parm", id: 10, price: 1000, count: 1, picture: pic10 },
+    { title: "farzandan", id: 11, price: 1000, count: 1, picture: pic11 },
+    { title: "lamber", id: 12, price: 1000, count: 1, picture: pic12 },
+    { title: "madam", id: 13, price: 1000, count: 1, picture: pic13 },
+    { title: "mashaal", id: 14, price: 1000, count: 1, picture: pic14 },
+    { title: "mojezeh", id: 15, price: 1000, count: 1, picture: pic15 },
+    { title: "jerminal", id: 16, price: 1000, count: 1, picture: pic16 },
+    { title: "vatican", id: 17, price: 1000, count: 1, picture: pic17 },
+    { title: "comedy", id: 18, price: 1000, count: 1, picture: pic18 },
+    { title: "confocious", id: 19, price: 1000, count: 1, picture: pic19 },
   ],
 };
 
