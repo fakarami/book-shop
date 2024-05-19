fb00ee3e4b31d7c59fd9b057390b87c96888f9c4
Faezeh-Karami <fk96karami@gmail.com>
2024-05-20 03:13:02 +0330

    set Raiting component

--- src/laouts/main/pages/shopPage/Raiting.jsx
+++ src/laouts/main/pages/shopPage/Raiting.jsx
@@ -1,5 +1,19 @@
 import React from "react";
-
+import Rating from "@mui/material/Rating";
+import Typography from "@mui/material/Typography";
 export default function Raiting() {
-  return <div>Raiting</div>;
+  const [value, setValue] = React.useState(2);
+  return (
+    <div>
+      <Typography component="legend">Controlled</Typography>
+      <Rating
+        name="simple-controlled"
+        value={value}
+        dir="ltr"
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      />
+    </div>
+  );
 }
