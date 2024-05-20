5bb70e6c05269d2f3e040d81033c8fdfd5c48954
Faezeh-Karami <fk96karami@gmail.com>
2024-05-20 21:54:46 +0330

    Edit Cart Page

--- src/laouts/main/personal menu/CartPage.jsx
+++ src/laouts/main/personal menu/CartPage.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import React from "react";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import { useSelector } from "react-redux";
+import Raiting from "../pages/shopPage/Raiting";
 
 export default function CartPage() {
+  const books = useSelector((state) => state.shop.books);
   return (
-    <div>CartPage</div>
-  )
+    <>
+      <div className="flexCard">
+        {books.map((book) =>
+          book.count > 0 ? (
+            <Card sx={{ maxWidth: 345 }} key={book.id}>
+              <CardMedia
+                component="img"
+                alt={book.title}
+                height="250"
+                image={book.picture}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h6" component="div">
+                  {book.title}
+                </Typography>
+                <hr />
+                <Raiting />
+              </CardContent>
+              <CardActions>
+                <Button size="large">+</Button>
+                {book.count}
+                <Button size="large">-</Button>
+              </CardActions>
+            </Card>
+          ) : (
+            ""
+          )
+        )}
+      </div>
+    </>
+  );
 }
