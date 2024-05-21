b845f1942afbbb86531638d357a7db53300ba911
Faezeh-Karami <fk96karami@gmail.com>
2024-05-21 20:08:12 +0330

    Edit AboutUs

--- src/laouts/main/pages/aboutUsPage/AboutUs.jsx
+++ src/laouts/main/pages/aboutUsPage/AboutUs.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import { Box, Paper } from "@mui/material";
+import React from "react";
 
 export default function AboutUs() {
   return (
-    <div>AboutUs</div>
-  )
+    <div>
+      <Box
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          "& > :not(style)": {
+            m: 1,
+            margin: "auto",
+            padding: 7,
+          },
+        }}
+      >
+        <Paper>
+          بسیار خرسندیم که میزبان شما در خانه کتاب هستیم.
+          <br /> خانه کتاب, شعبه مجازی فروشگاه‌ کتاب است.
+          <ul>
+            امکان خرید اینترنتی کالاهای فرهنگی از طریق سایت خانه کتاب در طبقه
+            بندی زیر فراهم است:
+          </ul>
+          <li> محصولات دیداری و شنیداری در حوزه موسیقی، فیلم و تاتر</li>
+          <li> نوشت افزار و کمک آموزشی</li>
+          <li> کتاب</li>
+          <li> دست‌سازه های هنری</li>
+        </Paper>
+      </Box>
+    </div>
+  );
 }
